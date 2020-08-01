a449b2033704a104ce77dee0ad304da8897f37a2
xinooo <cody9906121@gmail.com>
2020-08-01 15:13:11 +0800

    test4

--- app/src/main/java/com/example/an/num/MainActivity.java
+++ app/src/main/java/com/example/an/num/MainActivity.java
@@ -43,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private AlertDialog choiceDialog;
     private String aaa = "";
     private String ddd = "";
+    private String sss = "";
+    private String qqq = "";
 
 
 
